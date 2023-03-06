1c041997899f1363bc7cc3ceca2acbc8576e43cf
Omanshu Aman <omanshuaman@gmail.com>
2023-03-06 20:58:10 +0530

    ede

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://ivypods-frontend.onrender.com",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
